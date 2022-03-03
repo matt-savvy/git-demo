f907fca92679026be04a032653299f3d8e49f6dd
Matt Savoia <matt@1-800-rad-dude.com>
2022-03-02 23:23:52 -0500

    Initial word game

---
+++ index.js
@@ -0,0 +1,28 @@
+function isEven(number) {
+   return number % 2 === 0;
+}
+
+function isVowel(letter) {
+    if (['a', 'e', 'i', 'o', 'u'].includes(letter)) {
+        return true;
+    }
+
+    return false;
+}
+
+function wordWins(word) {
+    const length = word.length;
+    const firstLetter = word[0];
+
+    return isEven(length) && isVowel(firstLetter);
+}
+
+function main() {
+    const words = process.argv.slice(2);
+
+    words.forEach((word) => {
+        console.log(word, wordWins(word));
+    });
+}
+
+main();
